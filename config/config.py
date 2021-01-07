edbc6614343577cce094eb229e40b91c0b9b3084
jonjacobs24 <jonjacobs24@gmail.com>
2021-01-06 21:52:40 -0800

    started initial process of CNN training, image processing, and batch processing

--- config/config.py
+++ config/config.py
@@ -8,3 +8,5 @@ PACKAGE_ROOT = pathlib.Path().resolve()
 CNN_DIR = PACKAGE_ROOT / 'cnn_trained'
 TRAIN_DIR = PACKAGE_ROOT / 'data/train'
 TEST_DIR = PACKAGE_ROOT / 'data/test'
+
+TRAIN_IMAGE_FILE = 'training_images.npz'
